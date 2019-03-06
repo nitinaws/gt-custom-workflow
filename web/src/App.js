76537fb4a9d8d0ea4fa134af2f2e8eb72876fbb0
Ted Lee <sanghunl@amazon.com>
2019-03-06 10:22:00 -0800

    Code cleanup

--- web/src/App.js
+++ web/src/App.js
@@ -1,27 +1,16 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import { TokenAnnotator } from 'react-text-annotate'
 
-// const TEXT = `109277792 109277792 RWH 3477400 810976 7792902 01/30/2004 12:00:00 AM ED Discharge Summary Unsigned DIS ~~~ Report Status ~~~ Unsigned ED DISCHARGE NOTIFICATION / SUMMARY SHUFFSKOCKOTESKI, DRU ~~~ MRN ~~~ 3477400 ~~~ Age ~~~ 51y ~~~ REGISTRATION DATE ~~~ 01/30/2004 01:59 AM ~~~ Provider ~~~ KOMAND SILBEDOUETTLAND PRELIMINARY REPORT ~~~ PCP notified by MD ~~~ No - Other explanation ~~~ Benefits Assigned ~~~ N ~~~ Discharge Date / Time ~~~ 01/30/2004 08:28 ~~~ Discharge Status ~~~ Discharged ~~~ Condition on Discharge ~~~ Stable ~~~ Patient States Complaint ~~~ AGGRAVATED / NEEDS APS EVAL ~~~ Diagnosis ~~~ Impulsivity ~~~ 1` +
-//     '109277792 109277792 RWH 3477400 810976 7792902 01/30/2004 12:00:00 AM ED Discharge Summary Unsigned DIS ~~~ Report Status ~~~ Unsigned ED DISCHARGE NOTIFICATION / SUMMARY SHUFFSKOCKOTESKI, DRU ~~~ MRN ~~~ 3477400 ~~~ Age ~~~ 51y ~~~ REGISTRATION DATE ~~~ 01/30/2004 01:59 AM ~~~ Provider ~~~ KOMAND SILBEDOUETTLAND PRELIMINARY REPORT ~~~ PCP notified by MD ~~~ No - Other explanation ~~~ Benefits Assigned ~~~ N ~~~ Discharge Date / Time ~~~ 01/30/2004 08:28 ~~~ Discharge Status ~~~ Discharged ~~~ Condition on Discharge ~~~ Stable ~~~ Patient States Complaint ~~~ AGGRAVATED / NEEDS APS EVAL ~~~ Diagnosis ~~~ Impulsivity ~~~ 1'
-// const IMAGE_URL = "https://groundtruthnw.s3.amazonaws.com/raw-abstracts-jpgs/1801_00146.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20190225T225559Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Credential=AKIAISPQWY2DKP2O45RQ%2F20190225%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=b5ea9b21298e9780b56f48342a55e88c53e8a5ac44cbcf91e8abeff030b7c8d4"
-// const METADATA = "{\n" +
-//     "      \"payor_id\": \"Aetna\",\n" +
-//     "      \"claim_id\": \"133232\"\n" +
-//     "    }";
-
-
 const TEXT = document.querySelector('#document-text').innerText;
-const IMAGE_URL = document.querySelector('#encodedImage').innerText;
-const METADATA = document.querySelector('#metadata').innerText;
-
+const PDF_URL = document.querySelector('#encodedImage').innerText;
+const METADATA = JSON.parse(document.querySelector('#metadata').innerText);
 
 const TAG_COLORS = {
   ABSTRACT: '#84d2ff',
   TOPIC: '#00ffa2',
 }
 
-
-const Card = ({children}) => (
+const Card = ({ children }) => (
   <div
     style={{
       boxShadow: '0 2px 4px rgba(0,0,0,.1)',
@@ -34,51 +23,48 @@ const Card = ({children}) => (
   </div>
 )
 
-class App extends Component{
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            value: [],
-            tag: "ABSTRACT",
-            support_claim: "",
-            notes: "",
-            metadata: METADATA,
-            numPages: 1,
-            pageNumber: 1,
-        }
-    }
+    this.state = {
+       value: [],
+       tag: "ABSTRACT",
+       claimResult: false,
+       notes: "",
+       metadata: METADATA,
+       numPages: 1,
+       pageNumber: 1,
+    };
+  }
 
   handleChange = value => {
-    this.setState({value});
+    this.setState({ value });
   }
-
 
   handleTagChange = e => {
-    this.setState({tag: e.target.value})
+    this.setState({ tag: e.target.value });
   }
 
-  handleYesBtn = e => {
-    this.setState({support_claim: "YES"})
+  handleYesBtn = () => {
+    this.setState({ claimResult: true });
   }
 
-  handleNoBtn = e => {
-    this.setState({support_claim: "NO"})
+  handleNoBtn = () => {
+    this.setState({ claimResult: false });
   }
 
   handleNotes = e => {
-    this.setState( {notes: e.target.value})
+    this.setState({ notes: e.target.value });
   }
 
   render() {
     return (
+      <div style={{ padding: 12, fontFamily: 'sans-serif' }}>
 
-      <div style={{padding: 12, fontFamily: 'sans-serif'}}>
-
-        <div className="row" style={{fontFamily: 'sans-serif'}}>
-            <div class="col" style={{maxWidth:"1000px"}}>
-              <h3 style={{}}>Instructions</h3>
+        <div className="row" style={{ fontFamily: 'sans-serif' }}>
+            <div class="col" style={{ maxWidth: '1000px' }}>
+              <h3>Instructions</h3>
               <p> The task can be completed with blank, or saved and returned to when time is available to make more
                   progress.
                   If there is evidence in the record to support or deny an audit, <b>highlight</b> it with with the
@@ -87,48 +73,49 @@ class App extends Component{
             </div>
             <div></div>
         </div>
-        <div class="row" style={{display: 'flex', marginBottom: 6}}>
 
-
+        <div class="row" style={{ display: 'flex', marginBottom: 6 }}>
           <Card>
               <div class="img_contain">
-                <img id="claims_image" name="claims_image" class="page" src={IMAGE_URL} alt="alt"/>
+                <iframe src={PDF_URL} style={{ height: '500px' }} />
               </div>
           </Card>
 
           <Card>
-            <div class="col-sm-5" style={{paddingLeft:"0px"}}>
+            <div class="col-sm-5" style={{ paddingLeft:"0px" }}>
                 <select class="form-control" onChange={this.handleTagChange} value={this.state.tag}>
                     <option name="ABSTRACT" value="ABSTRACT">ABSTRACT</option>
                     <option name="TOPIC" value="TOPIC">TOPIC</option>
                 </select>
             </div>
+
             <div class="border border-success bg-light">
-                 <TokenAnnotator
-              style={{
-                fontFamily: 'sans-serif',
-                maxWidth: 600,
-                lineHeight: 1.5,
-              }}
-              tokens={TEXT.split(' ')}
-              value={this.state.value}
-              onChange={this.handleChange}
-              getSpan={span => ({
-                ...span,
-                tag: this.state.tag,
-                color: TAG_COLORS[this.state.tag],
-              })}
-              renderMark={props => (
-                <mark
-                  key={props.key}
-                  onClick={() => props.onClick({start: props.start, end: props.end})}
-                  color={TAG_COLORS[this.state.tag]}
-                >
-                  {props.content} [{props.tag}]
-                </mark>
-              )}
-            />
-              </div>
+             <TokenAnnotator
+                style={{
+                  fontFamily: 'sans-serif',
+                  maxWidth: 600,
+                  lineHeight: 1.5,
+                }}
+                tokens={TEXT.split(' ')}
+                value={this.state.value}
+                onChange={this.handleChange}
+                getSpan={span => ({
+                  ...span,
+                  tag: this.state.tag,
+                  color: TAG_COLORS[this.state.tag],
+                })}
+                renderMark={props => (
+                  <mark
+                    key={props.key}
+                    onClick={() => props.onClick({start: props.start, end: props.end})}
+                    color={TAG_COLORS[this.state.tag]}
+                  >
+                    {props.content} [{props.tag}]
+                  </mark>
+                )}
+              />
+            </div>
+
             <div class="form-row">
                 <div class="col-md-8">
                 <h5 class="font-weight-bold">Notes:</h5>
@@ -149,11 +136,7 @@ class App extends Component{
             </div>
           </Card>
         </div>
-          <pre hidden={true}>{JSON.stringify(this.state, null, 2)}</pre>
-          {/*<Card>*/}
-          {/*<h4>Current Value</h4>*/}
-          {/*<pre hidden={false}>{JSON.stringify(this.state, null, 2)}</pre>*/}
-        {/*</Card>*/}
+        <pre hidden>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     )
   }
